ecdb4beff21d946e08776e75880e4ed707b65188
Valerija Toncinska <valerija.toncinska12@gmail.com>
2024-03-11 17:53:25 +0300

    Merged 'Commands' and 'Manager', changed Main class

--- Runtime/Main.java
+++ Runtime/Main.java
@@ -1,6 +1,12 @@
 package CollectionProject.Runtime;
 
-import static CollectionProject.Runtime.Commands.runProgram;
+import CollectionProject.Object.Address;
+import CollectionProject.Object.Organization;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+import static CollectionProject.Object.OrganizationType.*;
 
 public class Main {
 
@@ -8,7 +14,84 @@ public class Main {
 
     public static void main(String[] args) {
 
-        runProgram();
+        System.out.println("Добро пожаловать в Collection Project!\n" +
+                "Для выхода из программы введите 'exit', для ознакомления со " +
+                "всеми доступными командами введите 'help'");
 
+        Scanner scanner = new Scanner(System.in); // VAl: создание сканера для считывания пользовательского ввода
+        String chosenCommand; // VAl: инициализация String, чтобы тот был виден в switch case'ах
+
+        /* VAL: для того, чтобы конструктор работал корректно вместе с полем postalAddress, т.к. это является объектом другого класса,
+        нам нужно вместо обычной строки писать в конструктор, например, вместо просто строки "Город Пушкин, улица Колотушкина",
+        писать new Address ("Город Пушкин, улица Колотушкина"), воооот */
+
+        Organization dunkinDonuts = new Organization("Dunkin Donuts", 66, PUBLIC, new Address("Город Пушкин, улица Колотушкина")); // сюда в Организацию он и вставляется, ну и так со всеми
+        Organization kFC = new Organization("KFC", 99, OPEN_JOINT_STOCK_COMPANY, null);
+        Organization waterSports = new Organization("Water Sports", 22, PUBLIC, new Address("Город Кульма, улица Улиткина"));
+
+        Address myNewBankAddress = new Address("Город Рига, улица Тескана");
+        Organization myNewBank = new Organization("My New Bank", 39, TRUST, myNewBankAddress);
+
+        // VAl: создание organizationHashtable, где ключом у нас является ID, а самой хранящейся информацией объект организации
+        Hashtable<Long, Organization> organizationHashtable = new Hashtable<>();
+
+        organizationHashtable.put(dunkinDonuts.getID(), dunkinDonuts); // VAl: кладём в таблицу id KFC с помощью геттера
+        organizationHashtable.put(kFC.getID(), kFC); // VAl: то же самое, что и выше
+        organizationHashtable.put(waterSports.getID(), waterSports); // VAl:то же самое, что и выше
+        organizationHashtable.put(myNewBank.getID(), myNewBank); // VAl: то же самое, что и выше
+
+
+        String value = String.valueOf(organizationHashtable.get(dunkinDonuts.getID()));
+        String value2 = String.valueOf(organizationHashtable.get(kFC.getID()));
+        String value3 = String.valueOf(organizationHashtable.get(waterSports.getID()));
+        String value4 = String.valueOf(organizationHashtable.get(myNewBank.getID()));
+        System.out.println(value + value2 + value3 + value4);
+
+        do {
+            /* VAl: создание цикла do, который будет вмещать в себя switch case для работы с программой,
+            после выбора команды и срабатывания определённого кейса, будет водить программу по кругу */
+
+            System.out.println("Введите желаемую вами команду");
+            chosenCommand = scanner.nextLine();
+
+
+            switch (chosenCommand) {
+                case "help":
+                    Manager.help();
+                    break;
+
+                case "info":
+                    Manager.info();
+                    break;
+
+                case "show":
+                    Manager.show(organizationHashtable);
+                    break;
+
+                case "insert":
+                    Manager.insert();
+                    break;
+
+                case "updateID":
+                    Manager.updateID();
+                    break;
+
+                case "removeKey":
+                    Manager.removeKey();
+                    break;
+
+                case "clear":
+                    Manager.clear();
+                    break;
+
+                case "replaceIfGreater":
+                    Manager.replaceIfGreater();
+                    break;
+
+                default:
+                    Manager.defaultError();
+            }
+
+        } while (!chosenCommand.equalsIgnoreCase("EXIT"));
     }
 }
--- Runtime/Manager.java
+++ Runtime/Manager.java
@@ -8,11 +8,16 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.Scanner;
 
+import static CollectionProject.Runtime.Utils.isInt;
+
 
 public class Manager {
     Scanner scanner;
-
     Map<Long, Organization> map;
+
+    Manager(){
+        this.map = new Hashtable<>();
+    }
 
     public static void defaultError() {
         System.out.println("Вы ввели неверные данные, прошу, проверьте вашу команду на наличие " +
@@ -20,83 +25,50 @@ public class Manager {
                 "\nВсе команды должны вводится маленькими буквами, без заглавных букв.");
     }
 
-
-    public static boolean isInt(String str) {
-        try {  // блок с которого все начнется тут
-            // если завершится с ошибкой, то перейдет в catche блок
-            Integer.valueOf(str);
-            // если предыдущая строка успешно отработала, то возвращаем true
-            return true;
-        } catch (Exception e) {
-            return false; // если попали в catch, то возвращаем false
-        }
+    public static void help() {
+        System.out.println("help: вывести описание всех команд \n" +
+                "info : информация о коллекции\n" +
+                "add : добавить новый элемент в коллекцию (спросить про значение каждого поля)\n" +
+                "update_by_id {id}: обновить значение элемента коллекции, id которого равен заданному\n" +
+                "remove_by_id {id} : удалить элемент из коллекции по его id\n" +
+                "clear : очистить коллекцию\n" +
+                "exit : завершить программу\n" +
+                "remove_head : вывести первый элемент коллекции и удалить его\n" +
+                "history : вывести последние 15 команд (без их аргументов)\n" +
+                "filter_less_than_balcony balcony : вывести элементы, значение поля balcony которых меньше заданного\n" +
+                "print_ascending : вывести элементы коллекции в порядке возрастания\n");
     }
 
-    // я не знаю, будет ли это работать и нужно ли это, но я это сделала, потом можно это поменять/удалить, воооот
-//    public static boolean isStr(int number) {
-//        try {  // блок с которого все начнется тут
-//            // если завершится с ошибкой, то перейдет в catche блок
-//            Integer.toString(number);
-//            // если предыдущая строка успешно отработала, то возвращаем true
-//            return true;
-//        } catch (Exception e) {
-//            return false; // если попали в catch, то возвращаем false
-//        }
-//    }
-
-//    public int getHouseYear() {
-//        while (true) {
-//            System.out.print("Введите название вашей организации: ");
-//            String line = scanner.nextLine();
-//
-//            if (!isInt(line)) {
-//                System.out.println("Это не число");
-//                continue;
-//            }
-//            int year = Integer.parseInt(line);
-//
-//            if (year < 2030 && year > 0) {
-//                return year;
-//            }
-//
-//            System.out.println("Должно быть меньше 2030 и больше 0");
-//        }
-//    }
-
-    public int getEmployeeNumberFromUser() {
-        while (true) {
-            System.out.print("Введите кол-во работников: ");
-            String line = scanner.nextLine();
-
-            if (!isInt(line)) {
-                System.out.println("Это не число");
-                continue;
-            }
-            int employeeNumber = Integer.parseInt(line);
-
-            if (employeeNumber > 0) {
-                return employeeNumber;
-            }
-
-            System.out.println("Не может быть пустым");
-        }
+    public static void info() {
+        System.out.println("Метод для инфо");
     }
 
-    public String getAddressFromUser() {
-        while (true) {
-            System.out.print("Введите адрес организации: ");
-            String addressLine = scanner.nextLine();
+    public static void show(Hashtable ht) {
+        System.out.println(ht.toString());
+    }
 
-            if (addressLine.length() > 191) {
-                System.out.println("Этот адрес слишком длинный, максимальное кол-во символов - 191");
-                continue;
-            }
-            return addressLine;
-        }
+    public static void insert() {
+        System.out.println("Метод для insert");
+    }
+
+    public static void updateID() {
+        System.out.println("Метод для updateID");
+    }
+
+    public static void removeKey() {
+        System.out.println("Метод для removeKey");
+    }
+
+    public static void clear() {
+        System.out.println("Метод для clear");
+    }
+
+    public static void replaceIfGreater() {
+        System.out.println("Метод для replaceIfGreater");
     }
 
     public void updateById(String id) {
-        if (!Utils.isInt(id)) {
+        if (!isInt(id)) {
             System.out.println("Значение id неправильное");
             return;
         }
@@ -143,7 +115,7 @@ public class Manager {
                 org.setType(orgType);
                 break;
             case EMPLOYEES_COUNT:
-                if (!Utils.isInt(newValue)) {
+                if (!isInt(newValue)) {
                     System.out.println("Указанно некорректное новое значение");
                     return;
                 }
@@ -153,6 +125,38 @@ public class Manager {
             case ADDRESS_STREET:
                 // ...
                 break;
+        }
+    }
+
+    public int getEmployeeNumberFromUser() {
+        while (true) {
+            System.out.print("Введите кол-во работников: ");
+            String line = scanner.nextLine();
+
+            if (!isInt(line)) {
+                System.out.println("Это не число");
+                continue;
+            }
+            int employeeNumber = Integer.parseInt(line);
+
+            if (employeeNumber > 0) {
+                return employeeNumber;
+            }
+
+            System.out.println("Не может быть пустым");
+        }
+    }
+
+    public String getAddressFromUser() {
+        while (true) {
+            System.out.print("Введите адрес организации: ");
+            String addressLine = scanner.nextLine();
+
+            if (addressLine.length() > 191) {
+                System.out.println("Этот адрес слишком длинный, максимальное кол-во символов - 191");
+                continue;
+            }
+            return addressLine;
         }
     }
 
